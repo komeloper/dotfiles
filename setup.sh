839c66822a8d3ebd21342918b912a0f5e36ff28d
komeloper <takumi.sakurai@creinolab.com>
2020-06-25 17:59:17 +0900

    test commit

---
+++ setup.sh
@@ -0,0 +1,19 @@
+DOT_DIRECTORY="${HOME}/dotfiles"
+SPECIFY_FILES=""
+
+for f in .??* ${SPECIFY_FILES}
+do
+    # 対象外のファイル、ディレクトリを設定
+    [[ ${f} == ".git" ]] && continue
+    [[ ${f} == ".DS_Store" ]] && continue
+
+
+    # シンボリックリンクを作成
+    ln -sfnv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+    echo ${f}
+
+done
+
+
+
+exit 0
